328f2932391540c7cfd5761caaa90b98989c963e
Anthony Rolland <6172844+LaconicPneumonic@users.noreply.github.com>
2021-07-25 15:47:18 -0400

    Add new texture

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="container">
-    <button @click="startPlaying">PLAY</button>
+    <button @click="startPlaying">START</button>
+    <button @click="stopPlaying">STOP</button>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import { GridGeometry } from "./grid";
 import { OrbitControls } from "./orbit";
 
 const test = require("/src/assets/test.mp3");
+const textureJpg = require("/src/assets/fiveTone.jpg");
 const binSize = 256;
 const gridWidth = 128;
 // get the average frequency of the sound
@@ -33,20 +35,38 @@ function init() {
   camera.position.z = 100;
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x050505);
+  scene.background = new THREE.Color(0x000000);
 
   //
 
-  const light = new THREE.HemisphereLight();
-  scene.add(light);
+  const ambientLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+  scene.add(ambientLight);
 
+  const width = 100;
+  const height = 100;
+  const intensity = 1;
+  const rectLight = new THREE.RectAreaLight(0xffffff, intensity, width, height);
+  rectLight.position.set(5, 5, 100);
+  rectLight.lookAt(0, 0, 0);
+  scene.add(rectLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 0.8);
+  camera.add(pointLight);
+  scene.add(camera);
+
+  const texture = new THREE.TextureLoader().load(textureJpg);
+
+  texture.minFilter = THREE.NearestFilter;
+  texture.magFilter = THREE.NearestFilter;
   //
 
-  geometry = new GridGeometry(10, gridWidth);
-  const material = new THREE.MeshBasicMaterial({
+  geometry = new GridGeometry(30, gridWidth);
+
+  const material = new THREE.MeshPhongMaterial({
+    // gradientMap: texture,
+    color: "#03fcdf",
     side: THREE.DoubleSide,
     vertexColors: true,
-    wireframe: true,
   });
 
   mesh = new THREE.Mesh(geometry, material);
@@ -70,7 +90,7 @@ function init() {
 
   stats = new Stats();
   stats.showPanel(1); // 0: fps, 1: ms, 2: mb, 3+: custom
-  document.body.appendChild(stats.dom);
+  // document.body.appendChild(stats.dom);
   window.addEventListener("resize", onWindowResize);
 }
 
@@ -87,71 +107,72 @@ let then = Date.now();
 let fpsInterval = 1000 / 30;
 const startTime = then;
 
-function animate(then) {
-  requestAnimationFrame(animate);
-
-  const now = Date.now();
-  const elapsed = now - then;
-
-  if (elapsed > fpsInterval) {
-    then = now - (elapsed % fpsInterval);
-    //
-    render(now - startTime);
-    controls.update();
-  }
-
-  stats.update();
-}
-
-function render() {
-  // geometry.pushRow(
-  //   Array.from({ length: gridWidth }).map(
-  //     (v, i) =>
-  //       5 * Math.sin((2 * Math.PI * (i - ((time / 1000) % 1000))) / gridWidth)
-  //   )
-  // );
-
-  geometry.pushRow(
-    analyzer
-      .freqDomain()
-      .slice(0, 128)
-      // .filter((_v, i) => i % (binSize / gridWidth) == 0)
-      .map((v) => v / 30)
-  );
-
-  renderer.render(scene, camera);
-}
-
 export default {
   name: "App",
   data: function () {
     return {
       init: false,
+      recording: false,
     };
   },
-  mounted: async function () {},
+  mounted: async function () {
+    init();
+
+    this.animate();
+
+    this.init = true;
+  },
   methods: {
     startPlaying: async function () {
       // create an Audio source\
 
-      if (analyzer) {
-        if (!analyzer.playing) {
-          analyzer.start();
-        } else {
-          analyzer.start(0);
-        }
-      } else {
+      if (!analyzer) {
         analyzer = (await Sound.input()).analyze(binSize);
-        // analyzer.start();
+      }
+      this.recording = true;
+    },
+    stopPlaying: async function () {
+      if (analyzer) {
+        if (analyzer.playing) {
+          analyzer.stop();
+        }
+      }
+      this.recording = false;
+    },
+    render: function () {
+      // geometry.pushRow(
+      //   Array.from({ length: gridWidth }).map(
+      //     (v, i) =>
+      //       5 * Math.sin((2 * Math.PI * (i - ((time / 1000) % 1000))) / gridWidth)
+      //   )
+      // );
+
+      if (this.recording) {
+        geometry.pushRow(
+          analyzer
+            .freqDomain()
+            .slice(0, 128)
+            .map((v) => v / 30)
+        );
       }
 
-      if (!this.init) {
-        init();
+      renderer.render(scene, camera);
+    },
 
-        animate();
+    animate: function (then) {
+      requestAnimationFrame(this.animate);
 
-        this.init = true;
+      const now = Date.now();
+      const elapsed = now - then;
+
+      if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+        //
+        this.render(now - startTime);
+        controls.update();
       }
+
+      stats.update();
     },
   },
 };
--- src/grid.js
+++ src/grid.js
@@ -12,8 +12,6 @@ class GridGeometry extends THREE.BufferGeometry {
             func: func,
         };
 
-        // generate vertices, normals and color data for a simple grid geometry
-
         this.lookup = [];
 
         for (let i = 0; i < this.parameters.segments; i++) {
@@ -62,12 +60,13 @@ class GridGeometry extends THREE.BufferGeometry {
                 const x = j * segmentSize - halfSize;
 
                 vertices.setXYZ(index, x, -y, this.lookup[i][j]);
-                // console.log(getXYZ(vertices, index))
                 index++;
 
-
-
                 normals.push(0, 0, 1);
+
+                const color = new THREE.Color("#FF00FF")
+                colors.push(color.r, color.g, color.b);
+
 
                 const r = x / this.parameters.size + 0.5;
                 const g = y / this.parameters.size + 0.5;
@@ -80,21 +79,13 @@ class GridGeometry extends THREE.BufferGeometry {
         for (let i = 0; i < this.parameters.segments - 1; i++) {
             for (let j = 0; j < this.parameters.segments - 1; j++) {
 
-                // Iterate through the first n-1 rows and columns i.e. (n)(n)
+                const a = i + this.parameters.segments * j;
+                const b = i + this.parameters.segments * (j + 1);
+                const c = (i + 1) + this.parameters.segments * (j + 1);
+                const d = (i + 1) + this.parameters.segments * j;
 
-                const first_index = i * (this.parameters.segments) + j;
-
-                const second_index = first_index + 1;
-
-                const third_index = first_index + this.parameters.segments;
-
-                const fourth_index = second_index + this.parameters.segments;
-
-                // console.log(first_index, second_index, third_index); // face one
-                // console.log(second_index, third_index, fourth_index); // face two
-
-                indices.push(first_index, second_index, third_index); // face one
-                indices.push(second_index, third_index, fourth_index); // face two
+                indices.push(a, b, d);
+                indices.push(b, c, d);
             }
         }
 
@@ -110,12 +101,14 @@ class GridGeometry extends THREE.BufferGeometry {
 
 
         this.getAttribute("position").needsUpdate = true;
+        this.getAttribute("color").needsUpdate = true;
     }
 
     update() {
 
 
         const vertices = this.getAttribute("position")
+        const colors = this.getAttribute("color")
 
 
         let index = 0;
@@ -125,7 +118,8 @@ class GridGeometry extends THREE.BufferGeometry {
             for (let j = 0; j < this.parameters.segments; j++) {
 
                 vertices.setZ(index, this.lookup[i][j]);
-                // console.log(this.getXYZ(vertices, index))
+                const color = new THREE.Color("#FF00FF")
+                colors.setXYZ(index, color.r, color.g, color.b)
                 index++;
 
 
@@ -133,6 +127,7 @@ class GridGeometry extends THREE.BufferGeometry {
         }
 
         this.getAttribute("position").needsUpdate = true;
+        this.getAttribute("color").needsUpdate = true;
         this.computeVertexNormals();
 
     }
